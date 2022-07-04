1446d9ffc6625810eba0fc8ebc04c5b781662d0e
Aleksey Andreev <fistashki22@gmail.com>
2022-07-04 17:01:09 +0300

    complete task 1

--- 7/Homework_7_Andreev.js
+++ 7/Homework_7_Andreev.js
@@ -1,58 +1,56 @@
 function Animal(name) {
-    var foodAmount = 50,
-        self = this;
-    function formatFoodAmount() {
-        return foodAmount + ' гр.';
+    this._foodAmount = 50;
+    this._name = name;
+}
+
+Animal.prototype._formatFoodAmount = function (foodAmount) {
+    return this._foodAmount + ' гр.';
+}
+
+Animal.prototype._dailyNorm = function (amount) {
+    if (!arguments.length) return this._formatFoodAmount();
+
+    if (amount < 30 || amount > 100) {
+        return 'Недопустимое количество корма.';
     }
 
-    self.dailyNorm = function (amount) {
-        if (!arguments.length) return formatFoodAmount();
+    this._foodAmount = amount;
+};
 
-        if (amount < 30 || amount > 100) {
-            return 'Недопустимое количество корма.';
-        }
-
-        foodAmount = amount;
-    };
-
-    self.name = name;
-
-    self.feed = function () {
-        console.log('Насыпаем в миску ' + self.dailyNorm() + ' корма.');
-    };
-}
+Animal.prototype._feed = function () {
+    console.log('Насыпаем в миску ' + this._dailyNorm() + ' корма.');
+    return this;
+};
 
 function Cat(name) {
     Animal.apply(this, arguments);
 
-    this.name = name;
-
-    var animalFeed = this.feed;
-    this.feed = function () {
-        animalFeed();
-        this.happyMessage();
-        return this;
-    }
-
-    this.happyMessage = function () {
-        console.log('Кот доволен ^_^');
-    };
-
-    this.stroke = function(){
-        console.log('Гладим кота.');
-        return this;
-    }
+    this._name = name;
 }
+
+Cat.prototype = Object.create(Animal.prototype);
+Cat.prototype.constructor = Cat;
+
+Cat.prototype._animalFeed = function () {
+    Animal.prototype._feed.apply(this);
+    console.log('Кот доволен');
+}
+
+Cat.prototype.stroke = function () {
+    console.log('Гладим кота.');
+    return this;
+}
+
 
 var barsik = new Cat('Барсик');
 
-console.log(barsik.dailyNorm());
-barsik.feed();
+console.log(barsik._dailyNorm());
+barsik._feed();
 
-console.log(barsik.dailyNorm(200));
-barsik.feed();
+console.log(barsik._dailyNorm(200));
+barsik._feed();
 
-console.log(barsik.dailyNorm(75));
-barsik.feed();
+console.log(barsik._dailyNorm(75));
+barsik._feed();
 
-barsik.feed().stroke().feed().stroke();
+barsik._feed().stroke()._feed().stroke();
